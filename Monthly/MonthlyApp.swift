68afbe92a6b814b47c82117c4844d2333acde050
Taha Chaudhry <taha.chaudhry@icloud.com>
2023-08-12 10:13:01 +0300

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Taha Chaudhry on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
